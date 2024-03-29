00eb00f5923dcfba17d7aeac9e5633cdd3897258
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-29 19:04:17 +0000

    ✨ feat: add function to prepare repository

---
+++ src/prepare-repository/index.ts
@@ -0,0 +1,21 @@
+import { Octokit } from '@octokit/core';
+import yargs from 'yargs';
+
+export default async function prepareRepository(argv: yargs.Arguments): Promise<void> {
+  const repoName = argv['repo-name'] as string;
+  const repoNameSnakeCase = repoName.split(' ').join('-').toLowerCase();
+  const humanReadableRepoName = repoName.replace(/-/g, ' ');
+  const baseURL = `/repos/kaiosilveira/${repoNameSnakeCase}-refactoring`;
+
+  const github = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const repoMetadata = {
+    description: `Working example with detailed commit history on the "${humanReadableRepoName}" refactoring based on Fowler's "Refactoring" book`,
+    homepage: 'https://github.com/kaiosilveira/refactoring',
+  };
+
+  const tags = ['javascript', 'refactoring', repoNameSnakeCase];
+
+  await github.request(`PATCH ${baseURL}`, repoMetadata);
+  await github.request(`PUT ${baseURL}/topics`, { names: tags });
+}
