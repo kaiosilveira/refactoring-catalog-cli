fdf8b162a558a9bb671e98cf3c8bda2d3b78c94c
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-30 08:12:26 +0000

    feat: add function to update README contents

---
+++ src/prepare-repository/update-readme/index.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+const humanizeRepoName = (repoName: string): string => {
+  return repoName
+    .split('-')
+    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+    .slice(0, -1)
+    .join(' ');
+};
+
+export function updateReadmeContents(markdown: string, repoName: string): string {
+  const result = markdown
+    .trim()
+    .replace(/refactoring-catalog-template/g, repoName)
+    .replace('# Refactoring name', `# ${humanizeRepoName(repoName)}`)
+    .replace(/\[REPOSITORY_NAME\]/g, repoName);
+
+  return result;
+}
+
+export default function updateReadmeFile(repoName: string): void {
+  const readmePath = path.join(process.cwd(), 'TEST.md');
+  const readmeContent = fs.readFileSync(readmePath, 'utf-8');
+  const updatedReadmeContent = updateReadmeContents(readmeContent, repoName);
+  fs.writeFileSync(readmePath, updatedReadmeContent);
+}
