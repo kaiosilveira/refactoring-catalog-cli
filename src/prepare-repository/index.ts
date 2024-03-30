735431fbc862abb23b2a4773a5da7130ac6758bf
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-30 08:14:37 +0000

    feat: update README as part of prepare-repository

--- src/prepare-repository/index.ts
+++ src/prepare-repository/index.ts
@@ -1,7 +1,9 @@
 import type * as yargs from 'yargs';
 import updateRepoPageOnGitHub from './update-repo-page-on-github';
+import updateReadmeFile from './update-readme';
 
 export default async function prepareRepository(argv: yargs.Arguments): Promise<void> {
   const repoName = argv['repo-name'] as string;
+  updateReadmeFile(repoName);
   await updateRepoPageOnGitHub(repoName);
 }
